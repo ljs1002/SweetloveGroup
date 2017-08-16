439e3ebbd67517e070b7e9ad97d4ee21bf5eebe3
sshameer <sanushameer@gmail.com>
2017-08-16 17:03:48 +0100

    structure updated

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
+dir_path = dir_path+"/SweetloveGroup/"
 
 setup(name='SweetloveGroup',
       version='1.0',
@@ -10,5 +11,7 @@ setup(name='SweetloveGroup',
       author='Sanu Shameer',
       author_email='sanushameer@gmail.com',
       url='https://github.com/sshameer/SweetloveGroup/',
-      packages=[dir_path+'../SweetloveGroup'],
+      py_modules=[dir_path+'/FVA',dir_path+'/FBA'],
+      license='GNU v3',
+      packages=[dir_path],
      )
