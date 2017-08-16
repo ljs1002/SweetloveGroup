7feb05b7481ddebdc5ec0e0aa218cd51995ac1bc
sshameer <sanushameer@gmail.com>
2017-08-16 15:16:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 
 from distutils.core import setup
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 setup(name='SweetloveGroup',
       version='1.0',
@@ -7,5 +10,5 @@ setup(name='SweetloveGroup',
       author='Sanu Shameer',
       author_email='sanushameer@gmail.com',
       url='https://github.com/sshameer/SweetloveGroup/',
-      packages=['../SweetloveGroup'],
+      packages=[dir_path+'../SweetloveGroup'],
      )
