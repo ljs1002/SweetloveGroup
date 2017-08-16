2e6784b39be81c86a547b6d4a83efca72b3b02c5
sshameer <sanushameer@gmail.com>
2017-08-16 15:11:14 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='SweetloveGroup',
+      version='1.0',
+      description='',
+      author='Sanu Shameer',
+      author_email='sanushameer@gmail.com',
+      url='https://github.com/sshameer/SweetloveGroup/',
+      packages=[../SweetloveGroup],
+     )
