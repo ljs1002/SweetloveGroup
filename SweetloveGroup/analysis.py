e100c760c067757d5f3be5e5e32adcec097690f0
sshameer <sanushameer@gmail.com>
2017-08-31 14:25:47 +0100

    Added function predictCCE

--- SweetloveGroup/analysis.py
+++ SweetloveGroup/analysis.py
@@ -47,6 +47,58 @@ def writeMetabSummary(cobra_model, met, Amet, outfile):
     fout.close()
       
       
+#Function to calculate night time carbon conversion efficiency in diel model
+#args: 1) a solved cobra model 2) day-night accumulation tag (default = "diel-
+#-Transfer") 3) reaction ID for night time output (default = phloem_output_tx-
+#-2), 4) reaction ID representing CO2 respired
+#output: carbon conversion efficiency
+def predictCCE(C3_model,accumulation_tag="dielTransfer",output="Phloem_output_tx2",CO2rxn = "CO2_tx2"):
+  import re
+  
+  for met in C3_model.metabolites:
+    if not met.formula:
+      met.formula=""
+  
+  Cin = 0
+  Cout = 0
+  
+  for rxn in C3_model.reactions.query(accumulation_tag):
+    if round(rxn.x,5)>0:
+      for met in rxn.products:
+        #print met
+        if met.formula.__contains__("C"):
+          #print str(Cin)+"---"+met.id
+          #print str(rxn.x)+"\t"+str(rxn.metabolites.get(met))+"\t"+str(int(re.split(r"[C,H]",met.formula)[1]))
+          Cin = Cin + (rxn.x * rxn.metabolites.get(met) * int(re.split(r"[C,H]",met.formula)[1]))
+  
+  for rxn in C3_model.reactions.query(accumulation_tag):
+    if round(rxn.x,5)<0:
+      for met in rxn.reactants:
+        if met.formula.__contains__("C"):
+          #print str(Cout)+"---"+met.id
+          #print str(rxn.x)+"\t"+str(rxn.metabolites.get(met))+"\t"+str(int(re.split(r"[C,H]",met.formula)[1]))
+          Cout = Cout + (rxn.x * rxn.metabolites.get(met) * int(re.split(r"[C,H]",met.formula)[1]))
+  
+  rxn = C3_model.reactions.get_by_id(output)
+  for met in rxn.reactants:
+    if met.formula.__contains__("C"):
+      #print str(Cout)+"---"+met.id
+      #print str(rxn.x)+"\t"+str(-1 * rxn.metabolites.get(met))+"\t"+str(int(re.split(r"[C,H]",met.formula)[1]))
+      Cout = Cout + (rxn.x * -1 * rxn.metabolites.get(met) * int(re.split(r"[C,H]",met.formula)[1]))
+  
+  Cout = Cout + (-1*C3_model.reactions.get_by_id(CO2rxn).x)
+  
+  if(not round(Cin,5) == round(Cout,5)):
+    print "Error, Cin = "+str(Cin)+" and Cout = "+str(Cout)
+    return 0
+  else:
+    print "Cin = "+str(Cin)+"\tCO2 ="+str(C3_model.reactions.get_by_id(CO2rxn).x)+"\t"+str(1 + ((C3_model.reactions.get_by_id(CO2rxn).x)/Cin))
+    return 1 + ((C3_model.reactions.get_by_id("CO2_tx2").x)/Cin)
+
+
+
+
+
 
 
   
