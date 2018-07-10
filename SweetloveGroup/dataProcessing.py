4064d879e57addc7f4c0aaee5b01d27ed180076c
Sanu Shameer <sanushameer@gmail.com>
2018-07-10 14:03:58 +0100

    Updated generateBoundsFromBiomass()
    added option to set number of random subsets and size of subsets

--- SweetloveGroup/dataProcessing.py
+++ SweetloveGroup/dataProcessing.py
@@ -1,18 +1,20 @@
-
-#This function processes raw biomass data and returns upper and lower bounds to 
-#be used in constraint scanning as done by Colombie et al 2015
-#	!!!Anaconda users need to uncomment %matplotlib inline!!!		                  #
-#			!!!Script is still in development!!!			                                #
-def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucrose",Rsampling = 1000):
+#################################################################################
+#This function processes raw biomass data and returns upper and lower bounds to #
+#be used in constraint scanning as done by Colombie et al 2015                  #
+#                                                                               #
+#################################################################################
+def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucrose",Nsampling = 1000,DPA=(11.4,14.8,18.2,21.6,25,28.4,31.8,25.2,38.6,41.3),show_plots=True,start=4.0,stop=57.0,degree=3,Ssampling=0.75):
   import pandas as pd
   import math
   import numpy as np
   
-  
+  conc=list()
+  rate_max=list()
+  rate_min=list()
+    
   #import data
   df = pd.read_csv(datafile,sep="\t")
-  Rsampling = 1000
-  num2rem = int(0.025*Rsampling)
+  num2rem = int(0.025*Nsampling)
   
   x_values = list()
   y_values = list()
@@ -30,20 +32,20 @@ def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucr
   
   
   
-  x2 = range(min(x_values),max(x_values)+1)
-  y2 = np.poly1d(np.polyfit(x_values,log_y_values,3))
+  x2 = np.arange(start,stop+0.1,0.1)
+  y2 = np.poly1d(np.polyfit(x_values,log_y_values,degree))
   
   ys = dict()
   
-  for i in range(0,Rsampling):
-    ind = np.random.choice(range(0,len(x_values)),size=50,replace=False)
+  for i in range(0,Nsampling):
+    ind = np.random.choice(range(0,len(x_values)),size=int(len(x_values)*Ssampling),replace=False)
     #print ind
     x=list()
     y=list()
     for j in ind:
       x.append(x_values[j])
       y.append(log_y_values[j])
-    ys[i]=np.poly1d(np.polyfit(x,y,3))
+    ys[i]=np.poly1d(np.polyfit(x,y,degree))
   
   
   maxys = list()
@@ -59,7 +61,7 @@ def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucr
     templist=list()
     for i in temp.keys():
       templist.append(temp[i][j])
-    temp2list=sorted(templist)[num2rem:Rsampling-num2rem]
+    temp2list=sorted(templist)[num2rem:Nsampling-num2rem]
     yi[j]=templist
     maxys.append(max(templist))
     max95s.append(max(temp2list))
@@ -91,28 +93,35 @@ def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucr
   for i in min95s:
     miny95_1.append(math.exp(i))
   
+  #print y3
+  #print "----"
+  #print x2
   
+  for i in DPA:
+    #print i
+    conc.append(y3[int((i-min(x_values))*10)])
   
-  
-  import matplotlib.pyplot as plt
-  #%matplotlib inline								#uncomment this line when using matplotlib
-  plt.rcParams.update({'font.size': 20}) #sets a global fontsize
-  plt.rcParams['xtick.major.size'] = 5 # adjusts tick line length and width
-  plt.rcParams['xtick.major.width'] = 1
-  plt.rcParams['ytick.major.size'] = 5
-  plt.rcParams['ytick.major.width'] = 1
-  plt.rcParams['axes.linewidth']=3 # makes axes line thicker
-  
-  
-  ax = plt.subplot()
-  ax.plot(x_values,y_values,".",label="raw data")
-  ax.plot(x2,y3,"g-",label="fitted curve")
-  ax.plot(x2,maxy1,"r--",label="fitted curve with max. y")
-  ax.plot(x2,miny1,"b--",label="fitted curve with min. y")
-  plt.xlabel("Time(days)")
-  plt.ylabel("Concentration (micromol/fruit)")
-  plt.legend(loc="best",fontsize=15)
-  plt.show()
+  if show_plots:
+    import matplotlib.pyplot as plt
+    %matplotlib inline
+    plt.rcParams.update({'font.size': 20}) #sets a global fontsize
+    plt.rcParams['xtick.major.size'] = 5 # adjusts tick line length and width
+    plt.rcParams['xtick.major.width'] = 1
+    plt.rcParams['ytick.major.size'] = 5
+    plt.rcParams['ytick.major.width'] = 1
+    plt.rcParams['axes.linewidth']=3 # makes axes line thicker
+    plt.figure(figsize=(5,5))
+    
+    
+    ax = plt.subplot()
+    ax.plot(x_values,y_values,".",label="raw data")
+    ax.plot(x2,y3,"g-",label="fitted curve")
+    ax.plot(x2,maxy1,"r--",label="fitted curve with max. y")
+    ax.plot(x2,miny1,"b--",label="fitted curve with min. y")
+    plt.xlabel("Time(days)")
+    plt.ylabel("Concentration (micromol/fruit)")
+    plt.legend(bbox_to_anchor=(2, 1),fontsize=15)
+    plt.show()
   
   ##############################derivatives#####################
   
@@ -129,7 +138,10 @@ def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucr
   miny95_deriv = list()
   temp=dict()
   for i in ys.keys():
-    temp[i]=ys_deriv[i](x2)
+    templist=list()
+    for j in range(0,len(x2)):
+      templist.append(y3[j]*ys_deriv[i](x2[j]))
+    temp[i]=templist
   
   
   yi_deriv = dict()
@@ -137,7 +149,7 @@ def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucr
     templist=list()
     for i in temp.keys():
       templist.append(temp[i][j])
-    temp2list=sorted(templist)[num2rem:Rsampling-num2rem]
+    temp2list=sorted(templist)[num2rem:Nsampling-num2rem]
     yi_deriv[j]=templist
     maxys_deriv.append(max(templist))
     maxy95_deriv.append(max(temp2list))
@@ -149,43 +161,46 @@ def generateBoundsFromBiomass(datafile="/home/sanu/ColobieDataRaw.csv",met="sucr
   
   y4 = list()
   for i in range(0,len(x2)):
-    y4.append(math.exp(y2(x2[i]))*y2_deriv(x2)[i])
+    y4.append(y3[i]*y2_deriv(x2)[i])
   
-  maxy2 = list()
-  maxy95_2 = list()
-  for i in range(0,len(x2)):
-    maxy2.append(maxy1[i]*maxys_deriv[i])
-    maxy95_2.append(maxy95_1[i]*maxy95_deriv[i])
-  
-  
-  miny2 = list()
-  miny95_2 = list()
-  for i in range(0,len(x2)):
-    miny2.append(miny1[i]*minys_deriv[i])
-    miny95_2.append(miny95_1[i]*miny95_deriv[i])
+  rate_max = list()
+  rate_min = list()
+  if not len(DPA)==0:
+    j=0
+    for i in DPA:
+      rate_max.append(maxy95_deriv[int((i-min(x_values))*10)])
+      rate_min.append(miny95_deriv[int((i-min(x_values))*10)])
+      j=j+1
   
   
+  if show_plots:
+    import matplotlib.pyplot as plt
+    
+    %matplotlib inline
+    plt.rcParams.update({'font.size': 20}) #sets a global fontsize
+    plt.rcParams['xtick.major.size'] = 5 # adjusts tick line length and width
+    plt.rcParams['xtick.major.width'] = 1
+    plt.rcParams['ytick.major.size'] = 5
+    plt.rcParams['ytick.major.width'] = 1
+    plt.rcParams['axes.linewidth']=3 # makes axes line thicker
+    plt.figure(figsize=(5,5))
+    
+    
+    ax = plt.subplot()
+    ax.plot(x2,y4,"g-",label="derivitative of fitted curve")
+    ax.plot(x2,maxys_deriv,"r-",label="derivative of fitted curve with max.y")
+    ax.plot(x2,minys_deriv,"b-",label="derivative of fitted curve with min.y")
+    ax.plot(x2,maxy95_deriv,"r--",label="derivative of fitted curve with max. y 95%")
+    ax.plot(x2,miny95_deriv,"b--",label="derivative of fitted curve with min. y 95%")
+    ax.set_title(met+" flux")
+    plt.xlabel("Time(days)")
+    plt.ylabel("Flux (micromol/fruit/day)")
+    plt.legend(bbox_to_anchor=(2.55, 1),fontsize=15)
+    plt.show()
   
-  import matplotlib.pyplot as plt
-  #%matplotlib inline								#uncomment this line when using matplotlib
-  plt.rcParams.update({'font.size': 20}) #sets a global fontsize
-  plt.rcParams['xtick.major.size'] = 5 # adjusts tick line length and width
-  plt.rcParams['xtick.major.width'] = 1
-  plt.rcParams['ytick.major.size'] = 5
-  plt.rcParams['ytick.major.width'] = 1
-  plt.rcParams['axes.linewidth']=3 # makes axes line thicker
-  
-  
-  ax = plt.subplot()
-  ax.plot(x2,y4,"g-",label="derivitative of fitted curve")
-  ax.plot(x2,maxy2,"r-",label="derivative of fitted curve with max.y")
-  ax.plot(x2,miny2,"b-",label="derivative of fitted curve with min.y")
-  ax.plot(x2,maxy95_2,"r--",label="derivative of fitted curve with max. y 95%")
-  ax.plot(x2,miny95_2,"b--",label="derivative of fitted curve with min. y 95%")
-  plt.xlabel("Time(days)")
-  plt.ylabel("Flux (micromol/fruit/day)")
-  plt.legend(loc="best",fontsize=15)
-  plt.show()
+  return (conc,rate_max,rate_min)
+
+
   
 ###################################################################
 #This function removes gene and protein associations from a given #
